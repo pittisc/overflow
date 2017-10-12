326ee3c754d099dfb92163ee74730c0c3e3f2358
dylan <dylan.mcguire@pitt.edu>
2017-10-12 10:51:14 -0400

    wrote vulnerable program

---
+++ vuln.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char buf[64];
+	printf("What is the password? ");
+	strcpy("%s", buf);
+	printf("WRONG! No shell for you!\n"); // haha silly user
+	return 0;
+}
