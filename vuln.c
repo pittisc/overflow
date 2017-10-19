45b6a8221bf47e0bda48fae7cb188df21679bfca
pittisc <pittinfosec@gmail.com>
2017-10-19 10:21:00 -0400

    modified some stuff

--- vuln.c
+++ vuln.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-    char buf[64];
-	printf("What is the password? ");
-	scanf("%s", buf);
+    char buf[256];
+	if (argc != 2) {
+		printf("USAGE: %s <password>\n", argv[0]);
+		return 1;
+	}
+	strcpy(buf, argv[1]);
 	printf("%s???\nWRONG! No shell for you!\n"); // haha silly user
 	return 0;
 }
